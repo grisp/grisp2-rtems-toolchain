17427fb52cc7af895fdda58b8b88b1288e8bc53e
Christian Mauderer <christian.mauderer@embedded-brains.de>
2020-07-31 09:02:45 +0200

    rtems: Switch to branches based on release.

--- debug/openocd/start-gdb.sh
+++ debug/openocd/start-gdb.sh
@@ -8,7 +8,7 @@ set -e -u
 # find out own directory
 SCRIPTPATH=$(readlink -- "$0" || echo "$0")
 SCRIPTDIR=$(CDPATH= cd -- "$(dirname -- "$SCRIPTPATH")" && pwd)
-PREFIX="${SCRIPTDIR}/../../rtems/6"
+PREFIX="${SCRIPTDIR}/../../rtems/5"
 export PATH="${PREFIX}/bin:${PATH}"
 
-arm-rtems6-gdb -x ${SCRIPTDIR}/start.gdb "$@"
+arm-rtems5-gdb -x ${SCRIPTDIR}/start.gdb "$@"
